22be649a153eea5fa984fd9e475c90b512495932
Alison Williams <apw5011@gmail.com>
2020-11-01 17:35:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,7 +118,7 @@ def play(board)
   end
   if won?(board)
     winner(board) == "X" || winner(board) == "O"
-    puts "Congratulations {winner(board)}!"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
